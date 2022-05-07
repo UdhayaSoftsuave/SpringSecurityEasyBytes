9dc1a2e828868f2026a25aaf60e4b5b3191732dc
@udhayakumar.thirunanasambantham <udhayakumar.thirunanasambantham@softsuave.com>
2022-05-07 12:48:35 +0530

    config InMemoryUserDetailsManager

--- src/main/java/com/example/SpringSecurityEasyBytes/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityEasyBytes/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.SpringSecurityEasyBytes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
@@ -25,17 +30,32 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		.httpBasic(); //it is allowed and get the request from the other backend application (RestTemplate) 
 	}
 
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//			.inMemoryAuthentication()
+//			.withUser("admin").password("admin").authorities("admin")
+//			.and()
+//			.withUser("user").password("user").authorities("user")
+//			.and()
+//			.passwordEncoder(NoOpPasswordEncoder.getInstance());
+//	} 
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-			.withUser("admin").password("admin").authorities("admin")
-			.and()
-			.withUser("user").password("user").authorities("user")
-			.and()
-			.passwordEncoder(NoOpPasswordEncoder.getInstance());
-	} 
+		InMemoryUserDetailsManager userservice = new InMemoryUserDetailsManager();
+		UserDetails user1 = User.withUsername("admin").password("admin").authorities("admin").build();
+		UserDetails user2 = User.withUsername("user").password("user").authorities("user").build();
+		userservice.createUser(user1);
+		userservice.createUser(user2);
+		
+		auth.userDetailsService(userservice);
+	}
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 	
 	
 	
