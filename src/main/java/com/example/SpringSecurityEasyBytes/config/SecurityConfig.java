98640ef6ca4b1da3823ea0eb21aa0737a378c9a6
@udhayakumar.thirunanasambantham <udhayakumar.thirunanasambantham@softsuave.com>
2022-05-07 11:52:36 +0530

    Config inMemoryAuthendication

--- src/main/java/com/example/SpringSecurityEasyBytes/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityEasyBytes/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.SpringSecurityEasyBytes.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
@@ -21,7 +23,20 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		.formLogin() //it allowed and get the request from the browser
 		.and()
 		.httpBasic(); //it is allowed and get the request from the other backend application (RestTemplate) 
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("admin").password("admin").authorities("admin")
+			.and()
+			.withUser("user").password("user").authorities("user")
+			.and()
+			.passwordEncoder(NoOpPasswordEncoder.getInstance());
 	} 
+	
+	
 	
 	
 
